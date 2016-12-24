3a85f5c79cc370727515f77895b29309aed3c87a
VasilisNNN <fillonikitaf@ukr.net>
2016-12-24 04:28:51 +0200

    qweq

---
+++ Assets/Code/Mirror.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mirror : MonoBehaviour {
+	public GameObject Set;
+	
+	public float 
+		X_Plus,
+		Y_Plus;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = Set.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GetComponent<Animator>()!=null)GetComponent<Animator>().SetBool ("Move", anim.GetBool("Move"));
+		transform.position = new Vector3 (Set.transform.position.x+X_Plus,Set.transform.position.y+Y_Plus,Set.transform.position.z);
+		transform.localScale = new Vector3 (Set.transform.localScale.x/1.09f,Set.transform.localScale.y/1.09f,1f);
+	}
+
+		
+
+}
---
+++ Assets/Code/Objects/PickObject.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickObject : MonoBehaviour {
+	
+    
+	public bool Picked {get;set;}	
+    public int IconNum;
+	private Inventory Inv;
+
+	
+void Start()
+	{ 
+		Inv = GameObject.Find("Vasilis").GetComponent<Inventory>();
+		Picked = false;	
+		//Load ();
+	}
+	
+private	void Update()
+	{
+
+	 
+      if(Picked)
+		{
+			if(Input.GetAxis("Enter")>0)
+			{
+			if(Inv.CheckEmpty()){
+			    PlayerPrefs.SetInt (name, -1);
+		        Inv.AddItem(IconNum);
+				//Inv.SaveInvDestroy(IconNum);
+				Inv.SaveInv();
+				Destroy(gameObject);
+			}
+
+			}
+		}
+		
+	}
+	private void OnTriggerStay2D(Collider2D e)
+	{
+		if(e.tag == "Player")
+		{
+		Picked = true;
+		}
+	}
+	
+		private void OnTriggerExit2D(Collider2D e)
+	{
+		if(e.tag == "Player")
+		{
+         Picked = false;
+		}
+	}
+
+	private void Load()
+	{
+		if (PlayerPrefs.GetInt (name) == -1)
+			Destroy (gameObject);
+	}
+	public string GetObName()
+	{
+		return gameObject.name;
+	}
+
+}
