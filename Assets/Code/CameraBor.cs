de95f66dbc23e1acd3d42795fb7020ae2ee3b0cb
VasilisNNN <fillonikitaf@ukr.net>
2016-12-24 04:47:32 +0200

    аполпоадлп

---
+++ Assets/Code/CameraBor.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBor : MonoBehaviour {
+
+	private float timer = 0;
+
+	private Transform PlayerV;
+	public static CameraBor Instance{get;private set;}
+	
+	private Vector2
+		Margin,
+		Smoothing;
+	public BoxCollider2D _bounds{ get; set;}
+	private GameObject[] CamBounds;
+	private Vector3
+		_min,
+		_max;
+	
+	private bool isFollowing;
+	public bool UpdateBounds = false; 
+	public bool StartPos{ get; set;}
+
+	private float x;
+	private float y;
+
+	private void Awake()
+	{
+		CamBounds = GameObject.FindGameObjectsWithTag ("CameraBound");
+		Margin.x = 1.2f;
+		Margin.y = 1.2f;
+		Smoothing.x = 0.8f;
+		Smoothing.y = 0.8f;
+		isFollowing = true;
+	}
+	public void Start()
+	{
+
+		PlayerV = GameObject.Find ("Vasilis").GetComponent<Transform> ();
+		x = PlayerV.position.x;
+		y = PlayerV.position.y;
+	 
+	  isFollowing = true;
+	  Application.targetFrameRate = 60;
+		//if(Player!=null) 
+	
+	
+
+	}
+
+
+	public void FixedUpdate()
+	{
+		foreach (GameObject c in CamBounds) {
+			if(c.GetComponent<BoxCollider2D> ().enabled)
+			_bounds = c.GetComponent<BoxCollider2D> ();
+		}
+
+		//if (UpdateBounds) {
+			SetVasPos();
+		//}
+
+		_min = _bounds.bounds.min;
+		_max = _bounds.bounds.max;
+
+
+		if(isFollowing)
+		{
+			if(Mathf.Abs(x-PlayerV.position.x)>Margin.x)
+				x = Mathf.Lerp(x,PlayerV.position.x,Smoothing.x * Time.deltaTime);
+			
+			if(Mathf.Abs(y-PlayerV.position.y)>Margin.y)
+				y = Mathf.Lerp(y,PlayerV.position.y,Smoothing.y * Time.deltaTime);
+			
+		}
+		
+		var cameraHalfWidth = Camera.main.orthographicSize * ((float)Screen.width/Screen.height);
+		
+
+			x = Mathf.Clamp (x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+			y = Mathf.Clamp (y, _min.y + Camera.main.orthographicSize, _max.y - Camera.main.orthographicSize);
+		
+		transform.position = new Vector3(x,y,-21);
+		
+	}
+
+
+	public void Set_UpdateBounds()
+	{
+		timer = Time.fixedTime+0.08f;
+
+	}
+
+	public void SetVasPos()
+	{
+
+
+		if (timer > Time.fixedTime) 
+		{
+			x = PlayerV.position.x;
+			y = PlayerV.position.y;
+		}
+
+	}
+
+}
---
+++ Assets/Code/Controll/DoorCol.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorCol : MonoBehaviour {
+	public bool EnterToTheDoor{ get; set; }
+	public string LevelName;
+	public bool DoorEnter = true;	
+	public bool DoorColl;
+	public bool LoadLocation = true;
+	private Inventory Inv;
+
+	public bool NoButtonEnter = false;
+
+	private bool SW;
+
+	
+private void Start()
+	{
+
+		if(GameObject.Find("Vasilis")!=null)
+		Inv = GameObject.Find("Vasilis").GetComponent<Inventory>();
+
+		//Inv.AddItem (45);
+		//vas = GameObject.Find("Vasilis").GetComponent<Transform>();
+
+		/*if (!NoButtonEnter&&LevelName==PlayerPrefs.GetString ("PrevName")) 
+		{
+			vas.position = new Vector3(PlayerPrefs.GetFloat("PrevX"),PlayerPrefs.GetFloat("PrevY"));
+		}*/
+
+	}
+	
+private void Update()
+	{	
+		//print (DoorColl);
+		if (!GetComponent<BoxCollider2D> ().enabled)
+			DoorColl = false;
+	//	print ("s "  + PlayerPrefs.GetString ("CorrLevel"));
+		if (DoorEnter) {
+				
+						if (NoButtonEnter && DoorColl) {
+			
+				EnterToTheDoor = true;
+				Inv.SaveInv ();
+
+				               
+
+								PlayerPrefs.SetString ("PrevName", Application.loadedLevelName);
+				           PlayerPrefs.SetString ("CorrLevel", LevelName);
+				if(LoadLocation){Application.LoadLevel (LevelName);
+					PlayerPrefs.SetString ("CorrLoadingLevel", LevelName);
+				}
+								
+			}else EnterToTheDoor = false;
+			
+			if (Input.GetButtonDown("Enter") && DoorColl
+			    &&
+			    GetComponent<BoxCollider2D>().enabled == true)
+			{
+
+				/*PlayerPrefs.SetFloat("PrevX",vas.position.x);
+				PlayerPrefs.SetFloat("PrevY",vas.position.y);
+*/
+				if(Application.loadedLevelName == "StartRoom")
+					PlayerPrefs.SetInt ("StartBedRoom", 1);
+
+				//Inv.SaveInv ();
+				             	PlayerPrefs.SetString ("PrevName", Application.loadedLevelName);
+				              PlayerPrefs.SetString ("CorrLevel", LevelName);
+
+				if(!LoadLocation)
+				{
+
+					if(Camera.main.GetComponent<CameraBor>()!=null)Camera.main.GetComponent<CameraBor>().Set_UpdateBounds();
+
+
+				}
+				if(LoadLocation)
+				{
+
+					if(Camera.main.GetComponent<CameraBor>()!=null)Camera.main.GetComponent<CameraBor>().Set_UpdateBounds();
+					Application.LoadLevel (LevelName);
+					PlayerPrefs.SetString ("CorrLoadingLevel", LevelName);
+				}
+
+						}
+				}
+
+
+	}
+
+	void OnTriggerStay2D(Collider2D c)
+	{
+		if (c.gameObject.tag == "Player") {
+			DoorColl = true;
+		} 
+	}
+	void OnTriggerExit2D(Collider2D c)
+	{
+		if (c.gameObject.tag == "Player") {
+
+			DoorColl = false;
+		} 
+	}
+	public void SetDoorEnter(bool DE)
+	{
+		DoorEnter = DE;
+	}
+	public bool GetDoorEnter()
+	{
+		return DoorEnter;
+	}
+
+
+
+
+}
